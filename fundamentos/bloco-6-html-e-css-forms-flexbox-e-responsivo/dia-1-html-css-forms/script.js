d7c57b4100a264bd4715c872b5f52c001bc62ce4
ThiagoTrybe19 <thiagoavels1@gmail.com>
2022-01-06 22:34:50 -0300

    Exercicios 6.1/6.2

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,3 +1,57 @@
+let picker = new Pikaday({
+    field: document.getElementById('data-inicio'),
+    format: 'D/M/YYYY',
+    toString(date, format) {
+        // you should do formatting based on the passed format,
+        // but we will just return 'D/M/YYYY' for simplicity
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+        // dateString is the result of `toString` method
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+    }
+});
+
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ])
+  .addField('#message', [
+    {
+      validator: (value) => {
+        return value[0] === '!';
+      },
+    },
+  ])
+
+
+
 let obj = { 
     0:  " AL ",   
     1:  " AP ",   
@@ -36,23 +90,15 @@ for (let i = 0; i<27; i++) {
     selected.appendChild(option)
 }
 
+/*
 let data = document.querySelector("#data")
 
 data.addEventListener("keypress", digitoData)
 
-
 function digitoData(DIGITO) { 
-    let numero = parseInt(DIGITO.key)
+      let numero = parseInt(DIGITO.key)
     if(numero != 0 && numero != 1 && numero != 2 && numero != 3 && numero != 4 && numero != 5 && numero != 6 && numero != 7 && numero != 8 && numero != 9 && DIGITO.key != '/') {
         DIGITO.preventDefault()
     }
-
 }
-/*
-O dia deve ser > 0 e <= 31.
-O mês deve ser > 0 e <= 12.
-O ano não pode ser negativo.
-Caso alguma das condições seja inválida no momento do envio do formulário, 
-exibir via alert uma mensagem de erro contextualizada.
-Campo obrigatório
 */
